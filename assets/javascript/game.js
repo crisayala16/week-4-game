374b0f0240c207dc8d07ea0dd5b81a71c5028c99
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-02-27 17:49:48 -0500

    finished file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
 	var lossOutput = $("#loss-output");
 	var userScoreOutput = $("#user-score-output");
 	var userScore = 0;
-
-	var randomNumber = function(){
-		return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-	}
+	var win = 0;
+	var loss = 0;
+	var randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+	
 
 	var randomCrystalNumber = function(){
 		return Math.floor(Math.random() * 12) + 1;
@@ -26,14 +26,50 @@ $(document).ready(function(){
 		userScore += crys.value;
 		userScoreOutput.text(userScore);
 	}
+	function reset(){
+		crystal1 = new crystal;
+		crystal2 = new crystal;
+		crystal3 = new crystal;
+		crystal4 = new crystal;
+		randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+		randomNumberOutput.html(randomNumber);
+		userScore = 0;
+		userScoreOutput.text(userScore);
+	}
+	function checkScore(){
+		if(userScore === randomNumber){
+			win++;
+			winOutput.text(win);
+			reset();
+		}
+		if(userScore > randomNumber){
+			loss++;
+			lossOutput.text(loss);
+			reset();
+		}
+	}
 
 	// $("#crystal-1").on("click", addValue(crystal1));
 
 	$("#crystal-1").on("click", function(){
 		userScore += crystal1.value;
 		userScoreOutput.text(userScore);
+		checkScore();
 	});
-
-
+	$("#crystal-2").on("click", function(){
+		userScore += crystal2.value;
+		userScoreOutput.text(userScore);
+		checkScore();
+	});
+	$("#crystal-3").on("click", function(){
+		userScore += crystal3.value;
+		userScoreOutput.text(userScore);
+		checkScore();
+	});
+	$("#crystal-4").on("click", function(){
+		userScore += crystal4.value;
+		userScoreOutput.text(userScore);
+		checkScore();
+	});
 	randomNumberOutput.html(randomNumber);
 })
